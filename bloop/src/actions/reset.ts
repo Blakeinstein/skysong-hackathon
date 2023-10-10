5dc9ca746c4e74e703c438bb0c2094762dc01dd6
Blakeinstein <rishi1998@gmail.com>
2023-10-10 12:37:28 -0700

    fix reset

--- bloop/src/actions/reset.ts
+++ bloop/src/actions/reset.ts
@@ -1,13 +1,29 @@
 'use server';
 
-import { promiseFs } from 'node:fs/promises';
 import { PINECONE_INDEX_NAME } from "@/config/pinecone";
+import fs from "node:fs/promises";
+
+import { tempPath } from "@/config/temp";
 import { pinecone } from "@/utils/pinecone-client";
-import { PineconeStore } from "langchain/vectorstores/pinecone";
+import path from "node:path";
+import { sleep } from "@/utils/sleep";
 
 export async function reset() {
-  const idx = pinecone.index(PINECONE_INDEX_NAME);
-  await idx.deleteAll();
+  try {
+    for (const file of await fs.readdir(tempPath)) {
+      await fs.unlink(path.join(tempPath, file));
+    }
+
+    await pinecone.deleteIndex(PINECONE_INDEX_NAME);
+    await sleep(10000);
+    await pinecone.createIndex({
+      name: PINECONE_INDEX_NAME,
+      dimension: 1536,
+      metric: "cosine",
+    });
+  } catch (e) {
+    console.log(e);
+  }
 
   return true;
 }
